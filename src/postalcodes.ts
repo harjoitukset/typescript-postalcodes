748fea648e6b8b7248392833f8062844d407f62d
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2023-01-10 16:29:12 +0000

    Update example code

--- src/postalcodes.ts
+++ src/postalcodes.ts
@@ -1,11 +1,12 @@
 import { readFileSync } from 'fs';
 import path from 'path';
 
-// you can generate the path to the CSV with the `path` module and `__dirname` variable:
-const csvFile = path.join(__dirname, '..', 'postalcodes.csv');
+// you can generate the relative path to the CSV that is in the parent folder of this file:
+const csvFile: string = path.join(__dirname, '..', 'postalcodes.csv');
 
-// file can be read into a string with the readFileSync function:
-let fileContents = readFileSync(csvFile, 'utf-8');
+// file can be read into a string with the `readFileSync` function:
+let fileContents: string = readFileSync(csvFile, 'utf-8');
+
 
 // the string can be split into lines with `split`:
 let lines: string[] = fileContents.trim().split('\n');
@@ -13,7 +14,8 @@ let lines: string[] = fileContents.trim().split('\n');
 console.log('The first 5 lines read from CSV file:');
 console.table(lines.slice(0, 5));
 
-// you can access the arguments via `process.argv` variable:
+
+// you can access command line arguments via `process.argv` variable:
 let params: string[] = process.argv;
 
 console.log('The contents of the `process.argv` array:');
